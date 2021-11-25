72547a45ae90b85dee864e2c37898e96ea46a298
Esther Kang <eskang87@gmail.com>
2021-11-24 16:37:31 -0800

    commit tree 71f7d1934d45841ab97ef82e8ca7f9508041844d

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+    if (distance > 42){
+        return distance - 42;
+    } else if (distance < 42) {
+        return 42 - distance;
+    }
+};
+
+function distanceFromHqInFeet(num) {
+    let blockNumber = distanceFromHqInBlocks(num);
+    return blockNumber * 264;
+};
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start) {
+    return (destination - start) * 264;
+    } else if (start > destination) {
+        return (start - destination) * 264;
+    }
+};
+
+function calculatesFarePrice(start, end) {
+    let feetDistance = distanceTravelledInFeet(start,end);
+    if (feetDistance <= 400) {
+        return 0;
+    } else if (feetDistance > 400 && feetDistance <= 2000){
+        return (feetDistance-400) * 0.02;
+    } else if (feetDistance > 2000 && feetDistance <= 2500){
+        return 25;
+    } else { 
+        return 'cannot travel that far';
+    }
+};
